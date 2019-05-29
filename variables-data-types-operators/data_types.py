752a293ea7e8fe2c8688b258829a67620cdbe076
Asad H. Ahmad <AAhmad@spartaglobal.com>
2019-05-29 10:43:03 +0100

    datatypes

---
+++ variables-data-types-operators/data_types.py
@@ -0,0 +1,41 @@
+# Numerical Types
+# numerical types are numbers, integers, floats or composites.
+# in python we will mostly deal with int and float. #
+
+num1 = 20
+
+print(type(num1))
+
+num2 = 20.0
+print(type(num2))
+
+# Declaring int or floats we use numerical characters without quotes.
+
+# Operators - we can add, subtract, devide and  multiply.
+result1 = 10 + 10 # add
+result2 = 10 - 2  #subtract
+result3 = 10 * 30 #multiply
+result4 = 20 % 2 #check for remainders
+
+print(result1, result2)
+
+# Logical Operators - to equate to something
+
+num_a = 10
+num_b = 10
+num_c = 13
+
+# bigger than
+print(num_b > num_c)
+# smaller than
+print(num_b < num_c)
+# Bigger than or equal
+print(num_b >= num_a)
+
+print(num_b <= num_c)
+
+# Check if its the same
+print(num_a == num_b)
+print(num_a == num_c)
+
+
---
+++ variables-data-types-operators/my_variable.py
@@ -0,0 +1,26 @@
+# a variable is alocated memory space to store something.
+# in python we can just declare variable by assigning them using the :
+# = sign
+
+
+# Convention:
+# Note: Naming is hard, but you should give meaningful names, Not x and y\
+# Lower case and use underscores
+
+# Creation and assignment of a variable
+my_variable = "I'm a variable"
+
+# Reassignment of variable
+
+my_variable = "Y not"
+
+print(my_variable)
+print(type(my_variable))
+
+another_var = 10
+
+print(another_var)
+print(type(another_var))
+
+
+
